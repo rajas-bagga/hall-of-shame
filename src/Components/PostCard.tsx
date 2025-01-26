321c892bc92e89c58cb7c5a25bc365f24f090ee2
New Name <new.email@example.com>
2025-01-26 23:08:26 +0530

    Completed the home page, the right section, the card section, and also added a sticky navbar at the bottom for mobile devices

---
+++ src/Components/PostCard.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faComment, faHeart } from "@fortawesome/free-regular-svg-icons";
+import { faTrophy } from "@fortawesome/free-solid-svg-icons";
+import { faPenNib } from "@fortawesome/free-solid-svg-icons";
+
+const PostCard = () => {
+  return (
+    <>
+      <div className="text-xl text-center mt-6 max-w-full w-100 m-auto rounded-md shadow-lg">
+        <div className="flex text-3xl h-15 items-center">
+          <div className="bg-black h-12 w-12 rounded-full m-2 border-3 border-purple-500">
+            <img
+              src="https://static.euronews.com/articles/stories/09/00/26/84/1200x675_cmsv2_5ab56c14-27b4-5b01-8d4f-5a2e91f2b20f-9002684.jpg"
+              className="rounded-full h-full w-full object-cover"
+              alt=""
+            />
+          </div>
+
+          <div className="font-mono text-xl">Vitamin Protein</div>
+        </div>
+
+        <div className="rounded-md">
+          <img
+            className="rounded-md w-full h-auto"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFy-2AF9QUct8T5VkY3VCpq7ghezF5oIZEGg&s"
+            alt=""
+          />
+        </div>
+        <div className="flex w-full py-2 justify-between">
+          <div className="flex">
+            <FontAwesomeIcon
+            //<FontAwesomeIcon icon="fa-solid fa-trophy" style={{color: "#d7a904",}} />
+              icon={faTrophy}
+              size="lg"
+              className="ml-2.5 hover:text-gray-400"
+            />
+            <p className="ml-2 font-mono">200</p>
+
+            <FontAwesomeIcon
+              icon={faPenNib}
+              size="lg"
+              className="ml-6 hover:text-gray-400"
+            />
+            <p className="ml-2 mr-5 font-mono">108</p>
+          </div>
+        </div>
+        <div className="text-sm text-gray-400 flex items-center mr-4 font-mono font-semibold ml-3 mb-2">
+          January 26, 2025
+        </div>
+        <div className="font-sans flex w-full text-left text-lg pl-3 pr-2 mb-2">
+          This is the description of this post, yeeeasfkjlsad fjsad flkjsad
+          flkajs Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Quidem, totam.
+        </div>
+        <div className="border-t-[0.5px] border-gray-500"></div>
+      </div>
+    </>
+  );
+};
+
+export default PostCard;
--- src/Core/Home.tsx
+++ src/Core/Home.tsx
@@ -1,6 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from "react";
 import "./Home.css";
 import HallButton from "../Components/HallButton";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faComment, faHeart } from "@fortawesome/free-regular-svg-icons";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import PostCard from "../Components/PostCard";
+import { faGear } from "@fortawesome/free-solid-svg-icons";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faCamera } from "@fortawesome/free-solid-svg-icons";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
 
 const Home = () => {
   return (
@@ -13,23 +22,62 @@ const Home = () => {
           <div className="w-full text-xl text-center mb-4">Place for logo</div>
           <HallButton />
         </div>
-        <div className="min-w-100">dfdsfsfasjdfksdhfadslasdfa</div>
 
-        <div className="min-w-60 w-60 flex-col  justify-start items-center h-screen hidden md:flex">
-          <div className="w-full text-xl">
-            <h1 className="mt-6 text-start">Search for hall</h1>
+        <div className="w-full flex-col justify-center items-center h-screen overflow-y-auto mr-1 pb-12">
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <PostCard />
+          <div
+            style={{ backgroundColor: "#121212" }}
+            className="md:hidden fixed bottom-0 w-full border-[0.25px] border-gray-500 text-center p-2 h-12 flex justify-around items-center"
+          >
+            <FontAwesomeIcon icon={faHouse} size="xl"/>
+            <FontAwesomeIcon icon={faMagnifyingGlass} size="xl"/>
+            <FontAwesomeIcon icon={faGear} size="xl"/>
+            <FontAwesomeIcon icon={faUser} size="xl"/>
+            <FontAwesomeIcon icon={faCamera} size="xl"/>
+          </div>
+        </div>
+
+        <div className="w-60 min-w-50 flex-col justify-start items-center h-screen hidden md:flex">
+          <div className="w-full h-30 flex justify-between items-center mt-1 pr-1.5">
+            {/* <div className="bg-purple-600 w-full h-10 rounded-md text-center flex justify-center align-middle items-center hover:bg-purple-500 cursor-pointer">
+              Henlo World
+            </div> */}
+            <div className="bg-purple-600 w-[48%] aspect-square rounded-md flex flex-col justify-center align-middle items-center hover:bg-purple-500 cursor-pointer">
+              <FontAwesomeIcon icon={faGear} size="2xl" className="mt-2" />
+              <p className="text-sm">Hall Settings</p>
+            </div>
+            <div className="bg-purple-600 w-[48%] aspect-square rounded-md flex flex-col justify-center align-middle items-center hover:bg-purple-500 cursor-pointer">
+              <FontAwesomeIcon icon={faUser} size="2xl" className="mt-2" />
+              <p className="text-sm">User Settings</p>
+            </div>
           </div>
 
-          <div className="flex justify-start w-full items-center">
+          <div className="w-full flex pr-1.5">
+            <div className="bg-purple-600 w-full h-18 rounded-md text-center flex flex-col justify-center align-middle items-center hover:bg-purple-500 cursor-pointer">
+              <FontAwesomeIcon icon={faCamera} size="2xl" className="mt-2" />
+              ShameShare
+            </div>
+          </div>
+
+          <div className="flex justify-start w-full items-center mt-1">
             <input
-              placeholder="Hall Code"
+              placeholder="Search for hall"
               type="text"
               className="border-1 w-6/7 rounded-md h-10 pl-1.5 mt-1 mr-1 pl focus:border-pink-600 focus:border-2 focus:outline-none"
             />
-            <button className="w-1/7 mt-0.5 text-3xl h-10 border-pink-600 border-2 rounded-md mr-1 cursor-pointer transition hover:bg-pink-600 duration-500 ease-in-out">⌕</button>
+            <button className="w-1/7 mt-0.5 text-3xl h-10 border-pink-600 border-2 rounded-md mr-1 cursor-pointer transition hover:bg-pink-600 duration-500 ease-in-out">
+              {/* ⌕ */}
+              <FontAwesomeIcon
+                icon={faMagnifyingGlass}
+                // size="lg"
+                size="xs"
+                className="ml-0.5"
+              />
+            </button>
           </div>
-
-          <div></div>
         </div>
       </div>
     </>
